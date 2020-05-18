a90ed9aba8eb796610a888853bc461ea5cf4bc14
minikiller <minikiller@qq.com>
2020-05-18 17:30:43 +0800

    login

--- src/_helpers/auth-header.js
+++ src/_helpers/auth-header.js
@@ -9,21 +9,21 @@ export function authHeader() {
     return {};
   }
 }
-import {userService} from '../_services'
+import { userService } from "../_services";
 export function handleResponse(response) {
-  return response.text().then(text => {
-      const data = text && JSON.parse(text);
-      if (!response.ok) {
-          if (response.status === 401) {
-              // auto logout if 401 response returned from api
-              userService.logout();
-              location.reload(true);
-          }
-
-          const error = (data && data.message) || response.statusText;
-          return Promise.reject(error);
+  return response.text().then((text) => {
+    const data = text && JSON.parse(text);
+    if (!response.ok) {
+      if (response.status === 401) {
+        // auto logout if 401 response returned from api
+        userService.logout();
+        if (location.pathname != "/login") location.reload(true);
       }
 
-      return data;
+      const error = (data && data.message) || response.statusText;
+      return Promise.reject(error);
+    }
+
+    return data;
   });
 }
