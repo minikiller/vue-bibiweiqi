99a9834d27afc3b550bd52e02f2e16f48e2b29c3
minikiller <minikiller@qq.com>
2020-05-19 21:51:44 +0800

    finish video

--- env.sh
+++ env.sh
@@ -1,3 +1,6 @@
 echo $NODE_API_ENV
 export NODE_API_ENV="http://bibiweiqi.com:5000"
 export NODE_SOCKET_ENV="https://bibiweiqi.com:3000"
+
+export NODE_API_ENV="https://192.168.1.34:5000"
+export NODE_SOCKET_ENV="https://192.168.1.34:3000"
--- src/play/PlayPage.vue
+++ src/play/PlayPage.vue
@@ -4,9 +4,7 @@
       <b-row>
         <b-col cols="8">
           <h2>
-            <b-badge v-if="game"
-              >{{ game.name }}--->{{ account.user.name }}</b-badge
-            >
+            <b-badge v-if="game">{{ game.name }}--->{{ account.user.name }}</b-badge>
           </h2>
           <div class="form-group">
             <button
@@ -14,13 +12,9 @@
               aria-pressed="true"
               class="btn btn-primary"
               @click="begin"
-            >
-              {{ btnText }}
-            </button>
+            >{{ btnText }}</button>
             <!-- <button class="btn btn-primary" @click="login">login</button> -->
-            <button class="btn btn-primary" @click="exit" ref="quit">
-              退出
-            </button>
+            <button class="btn btn-primary" @click="exit" ref="quit">退出</button>
           </div>
           <my-go
             v-if="game"
@@ -34,12 +28,12 @@
         <b-col cols="4">
           <b-tabs content-class="mt-3" justified>
             <b-tab title="视频" active>
-              <div>sterere</div>
-              <div class="">
+              <div class>
                 <vue-webrtc
                   ref="webrtc"
                   width="100%"
-                  :roomId="roomId"
+                  :userId="account.user.name"
+                  :roomId="game_id"
                   v-on:joined-room="logEvent"
                   v-on:left-room="logEvent"
                   v-on:open-room="logEvent"
@@ -50,30 +44,10 @@
               </div>
               <div class="row">
                 <div class="col-md-12 my-3">
-                  <button type="button" class="btn btn-primary" @click="onJoin">
-                    Join
-                  </button>
-                  <button
-                    type="button"
-                    class="btn btn-primary"
-                    @click="onLeave"
-                  >
-                    Leave
-                  </button>
-                  <button
-                    type="button"
-                    class="btn btn-primary"
-                    @click="onCapture"
-                  >
-                    Capture Photo
-                  </button>
-                  <button
-                    type="button"
-                    class="btn btn-primary"
-                    @click="onShareScreen"
-                  >
-                    Share Screen
-                  </button>
+                  <button type="button" class="btn btn-primary" @click="onJoin">Join</button>
+                  <button type="button" class="btn btn-primary" @click="onLeave">Leave</button>
+                  <button type="button" class="btn btn-primary" @click="onCapture">Capture Photo</button>
+                  <button type="button" class="btn btn-primary" @click="onShareScreen">Share Screen</button>
                 </div>
               </div>
               <!-- <my-video :gameId="game_id"></my-video> -->
@@ -82,10 +56,7 @@
               <chat v-if="game" :gameId="game_id" :gameInfo="game" />
             </b-tab>
             <b-tab title="观众">
-              <b-list-group
-                v-for="(user, index) in games.onlineUsers"
-                :key="index"
-              >
+              <b-list-group v-for="(user, index) in games.onlineUsers" :key="index">
                 <b-list-group-item>{{ user }}</b-list-group-item>
               </b-list-group>
             </b-tab>
@@ -107,15 +78,15 @@ import { socket, enable_board, initGameData } from "../_helpers";
 
 // import { WebRTC } from "plugin";
 import { find, head } from "lodash";
-import Vue from 'vue'
+import Vue from "vue";
 Vue.component(WebRTC.name, WebRTC);
 
 export default {
   computed: {
     ...mapState({
-      account: (state) => state.account,
-      games: (state) => state.games,
-    }),
+      account: state => state.account,
+      games: state => state.games
+    })
   },
   methods: {
     ...mapMutations("alert", ["success", "error", "clear"]),
@@ -127,12 +98,12 @@ export default {
         this.$refs.quit.disabled = true;
         this._socket.emit("prepareGame", {
           userId: this.account.user.name,
-          gameId: this.game_id,
+          gameId: this.game_id
         });
       } else if (this.btnText == "认输") {
         socket.emit("resign", {
           userId: this.account.user.name,
-          gameId: this.game_id,
+          gameId: this.game_id
         });
       }
     },
@@ -140,7 +111,7 @@ export default {
       console.log(`${this.account.user.name} is me`);
       socket.emit("logout", {
         userId: this.account.user.name,
-        gameId: this.game_id,
+        gameId: this.game_id
       });
       this.$router.push({ path: "/" });
     },
@@ -161,7 +132,7 @@ export default {
     },
     logEvent(event) {
       console.log("Event : ", event);
-    },
+    }
   },
   props: ["game_id"],
   data() {
@@ -176,12 +147,12 @@ export default {
       canBegin: false, //是否可以开始新对局
       kifu: "", //棋谱
       img: null,
-      roomId: "public-room",
+      roomId: "public-room"
     };
   },
   mounted() {
     this._socket = socket;
-    gameService.getById(this.game_id).then((data) => {
+    gameService.getById(this.game_id).then(data => {
       this.game = data;
       this.gameUser.push(this.game.blackone_id);
       this.gameUser.push(this.game.blacktwo_id);
@@ -201,11 +172,11 @@ export default {
       return data;
     });
     //棋手对局进入准备状态
-    EventBus.$on("prepare", (msg) => {
+    EventBus.$on("prepare", msg => {
       this.success(msg);
     });
     //棋局正式开始
-    EventBus.$on("beginGame", (game) => {
+    EventBus.$on("beginGame", game => {
       // this.success(msg);
       this.clear();
       this.btnText = "认输";
@@ -214,7 +185,7 @@ export default {
     });
 
     //棋局正式结束
-    EventBus.$on("resign", (msg) => {
+    EventBus.$on("resign", msg => {
       // this.success(msg);
       //TODO send kifu to server msg.kifu
       this.error(msg.result);
@@ -225,7 +196,7 @@ export default {
   components: {
     MyGo,
     Chat,
-    MyVideo,
-  },
+    MyVideo
+  }
 };
 </script>
