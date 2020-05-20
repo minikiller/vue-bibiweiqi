f78cece0ca094e15b8a1d63e45811f8f37bb5d69
hongyu7 <hongyu71@gmail.com>
2020-05-21 00:56:06 +0800

    .

--- src/_helpers/auth-header.js
+++ src/_helpers/auth-header.js
@@ -1,31 +1,31 @@
 export function authHeader() {
-  // return authorization header with jwt token
-  let user = JSON.parse(sessionStorage.getItem("user"));
+    // return authorization header with jwt token
+    let user = JSON.parse(sessionStorage.getItem("user"));
 
-  if (user && user.token) {
-    return { "x-access-token": user.token };
+    if (user && user.token) {
+        return { "x-access-token": user.token };
     // return { 'x-access-token': 'Bearer ' + user.token };
-  } else {
-    return {};
-  }
+    } else {
+        return {};
+    }
 }
 import { userService } from "../_services";
 export function handleResponse(response) {
-  return response.text().then((text) => {
-    const data = text && JSON.parse(text);
-    if (!response.ok) {
-      if (response.status === 401) {
-        // auto logout if 401 response returned from api
-        userService.logout();
-        if (location.pathname !== "/login") {
-          location.reload(true);
+    return response.text().then((text) => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401) {
+                // auto logout if 401 response returned from api
+                userService.logout();
+                if (location.pathname !== "/login") {
+                    location.reload(true);
+                }
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
         }
-      }
 
-      const error = (data && data.message) || response.statusText;
-      return Promise.reject(error);
-    }
-
-    return data;
-  });
+        return data;
+    });
 }
