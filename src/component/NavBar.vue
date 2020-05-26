e02ef2b484ace11db960c4b17d5854f057a7a970
hongyu7 <hongyu71@gmail.com>
2020-05-26 18:23:55 +0800

    没有登陆的时候不显示导航条。

--- src/component/NavBar.vue
+++ src/component/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="info">
+  <b-navbar v-if="account.user" toggleable="lg" type="dark" variant="info">
     <b-container>
       <b-navbar-brand href="#">{{ games.navTitle }}</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
