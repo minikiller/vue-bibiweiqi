26107302639f8433af1afc54b72a6c42da27ff33
minikiller <minikiller@qq.com>
2020-05-30 09:43:27 +0800

    add base64 function

--- src/component/ImageInput.vue
+++ src/component/ImageInput.vue
@@ -5,12 +5,14 @@
       <slot name="activator"></slot>
     </div>
     <!-- image input: style is set to hidden and assigned a ref so that it can be triggered -->
-    <input type="file"
-       ref="file"
-       :name="uploadFieldName"
-       @change="onFileChange(
+    <input
+      type="file"
+      ref="file"
+      :name="uploadFieldName"
+      @change="onFileChange(
           $event.target.name, $event.target.files)"
-       style="display:none">
+      style="display:none"
+    />
     <!-- error dialog displays any potential error messages -->
     <v-dialog v-model="errorDialog" max-width="300">
       <v-card>
@@ -25,46 +27,93 @@
 </template>
 
 <script>
-  export default {
-    name: 'image-input',
-    data: ()=> ({
-      errorDialog: null,
-      errorText: '',
-      uploadFieldName: 'file',
-      maxSize: 1024
-    }),
-    props: {
+export default {
+  name: "image-input",
+  data: () => ({
+    errorDialog: null,
+    errorText: "",
+    uploadFieldName: "file",
+    maxSize: 1024
+  }),
+  props: {
     // Use "value" to enable using v-model
-      value: Object,
-    },
-    methods: {
-      launchFilePicker(){
-        this.$refs.file.click();
-      },
-      onFileChange(fieldName, file) {
-        const { maxSize } = this
-        let imageFile = file[0]
+    value: Object
+  },
+  methods: {
+    getBase64Image(img) {
+      // Create an empty canvas element
+      var canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
 
-        if (file.length>0) {
-          let size = imageFile.size / maxSize / maxSize
-          if (!imageFile.type.match('image.*')) {
-            // check whether the upload is an image
-            this.errorDialog = true
-            this.errorText = 'Please choose an image file'
-          } else if (size>1) {
-            // check whether the size is greater than the size limit
-            this.errorDialog = true
-            this.errorText = 'Your file is too big! Please select an image under 1MB'
-          } else {
-            // Append file into FormData and turn file into image URL
-            let formData = new FormData()
-            let imageURL = URL.createObjectURL(imageFile)
-            formData.append(fieldName, imageFile)
-            // Emit the FormData and image URL to the parent component
-            this.$emit('input', { formData, imageURL })
-          }
+      // Copy the image contents to the canvas
+      var ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+
+      // Get the data-URL formatted image
+      // Firefox supports PNG and JPEG. You could check img.src to
+      // guess the original format, but be aware the using "image/jpg"
+      // will re-encode the image.
+      var dataURL = canvas.toDataURL("image/png");
+
+      return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    },
+    launchFilePicker() {
+      this.$refs.file.click();
+    },
+    toDataURL(url, callback) {
+      var xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        var reader = new FileReader();
+        reader.onloadend = function() {
+          callback(reader.result);
+        };
+        reader.readAsDataURL(xhr.response);
+      };
+      xhr.open("GET", url);
+      xhr.responseType = "blob";
+      xhr.send();
+    },
+    encodeImageFileAsURL(file) {
+      // var file = element.files[0];
+      var reader = new FileReader();
+      reader.onloadend = function() {
+        console.log("RESULT", reader.result);
+      };
+      reader.readAsDataURL(file);
+    },
+    onFileChange(fieldName, file) {
+      const { maxSize } = this;
+      let imageFile = file[0];
+      this.encodeImageFileAsURL(imageFile);
+      if (file.length > 0) {
+        let size = imageFile.size / maxSize / maxSize;
+        if (!imageFile.type.match("image.*")) {
+          // check whether the upload is an image
+          this.errorDialog = true;
+          this.errorText = "Please choose an image file";
+        } else if (size > 1) {
+          // check whether the size is greater than the size limit
+          this.errorDialog = true;
+          this.errorText =
+            "Your file is too big! Please select an image under 1MB";
+        } else {
+          // Append file into FormData and turn file into image URL
+          let formData = new FormData();
+          let imageURL = URL.createObjectURL(imageFile);
+          formData.append(fieldName, imageFile);
+          // let value = this.getBase64Image(imageFile);
+          this.toDataURL(
+            "https://www.gravatar.com/avatar/d50c83cc0c6523b4d3f6085295c953e0",
+            function(dataUrl) {
+              console.log("RESULT:", dataUrl);
+            }
+          );
+          // Emit the FormData and image URL to the parent component
+          this.$emit("input", { formData, imageURL });
         }
       }
     }
   }
+};
 </script>
