8e220d2938e5c069b0aec4c375f687bfc9c4b74d
hongyu7 <hongyu71@gmail.com>
2020-06-09 18:41:49 +0800

    .

--- src/game/GamePage.vue
+++ src/game/GamePage.vue
@@ -9,33 +9,26 @@
                 <b-col>
                   <b-row>
                     <b-col>
-                      <b-form-group id="input-group-0" label-for="input-0" label="对局室名称">
-                        <b-form-input
-                          id="input-0"
-                          v-model="form.name"
-                          required
-                          placeholder="输入对局室名称"
-                        ></b-form-input>
-                      </b-form-group>
+                      <b-input-group prepend="对局名称">
+                        <b-form-input id="input-0" v-model="form.name" required></b-form-input>
+                      </b-input-group>
                     </b-col>
                   </b-row>
                   <b-row>
                     <b-col>
-                      <b-form-group id="input-group-1" label="选择四位棋手" label-for="input-1">
+                      <b-form-group id="input-group-1" label="参赛棋手" label-for="input-1">
                         <v-select
                           multiple
                           id="input-1"
                           label="name"
-                          :filterable="true"
-                          :clearable="true"
+                          :filterable="false"
+                          :clearable="false"
                           :options="options"
                           @search="onSearch"
-                          :clearSearchOnSelect="false"
-                          placeholder="输入棋友名称，进行搜索"
                           v-model="form.opponent"
                           :selectable="() => form.opponent.length < 4"
                         >
-                          <template slot="no-options">未找到对应的记录</template>
+                          <template slot="no-options">输入棋友名称，进行搜索</template>
                           <template slot="option" slot-scope="option">
                             <div class="d-center">
                               <b-avatar variant="dark" :src="option.avatar" size="sm" />
@@ -46,60 +39,75 @@
                       </b-form-group>
                     </b-col>
                   </b-row>
-                  <b-row>
-                    <b-col class="avatars_l">
-                      <b-avatar badge="1" badge-variant="dark" size="3rem"></b-avatar>
+                  <b-row class="row_avatars">
+                    <b-col class="avatars">
                       <div v-if="form.opponent[0]">
-                        <b-avatar variant="dark" :src="form.opponent[0].avatar" size="sm" />
+                        <b-avatar
+                          badge="1"
+                          badge-variant="dark"
+                          :src="form.opponent[0].avatar"
+                          size="4rem"
+                        />
                         {{ form.opponent[0].name }}
                       </div>
+                      <div v-else>
+                        <b-avatar variant="primary" size="4rem">黑棋一</b-avatar>
+                      </div>
                     </b-col>
-                    <b-col class="avatars_r">
-                      <b-avatar badge="2" badge-variant="light" size="3rem"></b-avatar>
+                    <b-col class="avatars">
                       <div v-if="form.opponent[1]">
-                        <b-avatar variant="dark" :src="form.opponent[1].avatar" size="sm" />
+                        <b-avatar
+                          badge="1"
+                          badge-variant="light"
+                          :src="form.opponent[1].avatar"
+                          size="4rem"
+                        />
                         {{ form.opponent[1].name }}
                       </div>
-                    </b-col>
-                    <b-col class="avatars_l">
-                      <b-avatar badge="3" badge-variant="dark" size="3rem"></b-avatar>
-                      <div v-if="form.opponent[2]">
-                        <b-avatar variant="dark" :src="form.opponent[2].avatar" size="sm" />
-                        {{ form.opponent[2].name }}
+                      <div v-else>
+                        <b-avatar variant="primary" size="4rem">白棋一</b-avatar>
                       </div>
                     </b-col>
-                    <b-col class="avatars_r">
-                      <b-avatar badge="4" badge-variant="light" size="3rem"></b-avatar>
+                    <b-col class="avatars">
+                      <div v-if="form.opponent[2]">
+                        <b-avatar
+                          badge="2"
+                          badge-variant="dark"
+                          :src="form.opponent[2].avatar"
+                          size="4rem"
+                        />
+                        {{ form.opponent[2].name }}
+                      </div>
+                      <div v-else>
+                        <b-avatar variant="primary" size="4rem">黑棋二</b-avatar>
+                      </div>
+                    </b-col>
+                    <b-col class="avatars">
                       <div v-if="form.opponent[3]">
-                        <b-avatar variant="dark" :src="form.opponent[3].avatar" size="sm" />
+                        <b-avatar
+                          badge="2"
+                          badge-variant="light"
+                          :src="form.opponent[3].avatar"
+                          size="4rem"
+                        />
                         {{ form.opponent[3].name }}
+                      </div>
+                      <div v-else>
+                        <b-avatar variant="primary" size="4rem">白棋二</b-avatar>
                       </div>
                     </b-col>
                   </b-row>
                   <b-row>
                     <b-col>
-                      <b-form-group id="input-group-2" label-for="input-2" label="选择对局时长">
-                        <b-form-input
-                          id="input-2"
-                          required
-                          v-model="form.total_time"
-                          type="range"
-                          min="5"
-                          max="100"
-                          step="5"
-                        ></b-form-input>
-                        <!-- <b-form-spinbutton id="input-2" required v-model="form.total_time" min="5" max="100"></b-form-spinbutton> -->
-                        <div class="mt-2">对局时长: {{ form.total_time }}分钟</div>
-                      </b-form-group>
+                      <b-input-group prepend="对局时间" append="分钟">
+                        <b-form-input id="input-2" v-model="form.total_time" required></b-form-input>
+                      </b-input-group>
                     </b-col>
                   </b-row>
                   <b-row>
-                    <b-button type="submit" variant="primary">
-                      <i class="fa fa-shopping-cart"></i> 提交
-                    </b-button>&nbsp
-                    <b-button type="reset" variant="danger">
-                      <i class="fas fa-history"></i> 重置
-                    </b-button>
+                    <b-col>
+                      <b-button type="submit" block variant="success">提交</b-button>
+                    </b-col>
                   </b-row>
                 </b-col>
               </b-row>
@@ -128,7 +136,7 @@ export default {
       options: [],
       form: {
         opponent: [],
-        total_time: 5,
+        total_time: "",
         name: "",
         public: true,
         password: "",
@@ -158,10 +166,6 @@ export default {
 
     onSubmit(evt) {
       evt.preventDefault();
-      if (this.form.opponent.length == 0 || this.form.opponent.length < 4) {
-        this.error("请选择至少四位棋友！");
-        return;
-      }
       // alert(JSON.stringify(this.form));
       gameService.newGame(this.form).then(data => {
         this.success(data.message);
@@ -191,10 +195,15 @@ export default {
 .b-avatar {
   margin: 0.5rem;
 }
-.avatars_l {
-  padding: 0.25rem 0.25rem 0.25rem 0.75rem;
+.avatars {
+  padding: 0.5rem;
+
+  text-align: center;
 }
-.avatars_r {
-  padding: 0.25rem 0.75rem 0.25rem 0.25rem;
+.row_avatars {
+  padding-left: 4rem;
+  padding-right: 4rem;
+  height: 9rem;
+  text-align: center;
 }
 </style>
