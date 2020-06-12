6f17af5fca9775a6ab1e9f69d5b0ecb94e2c8c84
minikiller <minikiller@qq.com>
2020-06-12 16:22:46 +0800

    change style

--- src/component/MyGo.vue
+++ src/component/MyGo.vue
@@ -18,16 +18,34 @@
       <b-card-group deck>
         <b-card header-tag="header" footer-tag="footer">
           <!-- <b-card-title>{{ blackOne }}</b-card-title> -->
-          <template v-slot:header>
+          <!-- <template v-slot:header>
             <h6 class="mb-0">
               <b-avatar variant="info" :src="account.user.avatar"></b-avatar>
               {{ blackOne }}
             </h6>
             <div v-if="b1_turn" class="text-success">行棋</div>
-          </template>
-          <b-card-text>用时：{{ b_playTime }}</b-card-text>
-          <b-card-text>提子：</b-card-text>
-          
+          </template> -->
+          <b-card-text>
+            <b-row>
+              <b-col class="col-8">
+                <b-avatar variant="info" :src="account.user.avatar"></b-avatar>
+                {{ blackOne }} <br/>
+                <b-img src="/static/img/xingqi.png" fluid alt="Responsive image" width="59px"></b-img>
+              </b-col>
+              <b-col>
+                <b-row>
+                  <b-col>用时：{{ b_playTime }}</b-col>
+                </b-row>
+                <b-row>
+                  <b-col>提子：</b-col>
+                  <!-- <b-card-text>提子：</b-card-text> -->
+                </b-row>
+                <b-row>
+                  <b-col>读秒: {{ b_playTime }}</b-col>
+                </b-row>
+              </b-col>
+            </b-row>
+          </b-card-text>
         </b-card>
 
         <b-card header-tag="header" footer-tag="footer">
@@ -39,7 +57,6 @@
           </template>
           <b-card-text>用时：{{ w_playTime }}</b-card-text>
           <b-card-text>提子：</b-card-text>
-          
         </b-card>
       </b-card-group>
     </div>
@@ -54,7 +71,6 @@
           </template>
           <b-card-text>用时：{{ b_playTime }}</b-card-text>
           <b-card-text>提子：</b-card-text>
-         
         </b-card>
 
         <b-card header-tag="header" footer-tag="footer">
@@ -66,7 +82,6 @@
           </template>
           <b-card-text>用时：{{ w_playTime }}</b-card-text>
           <b-card-text>提子：</b-card-text>
-          
         </b-card>
       </b-card-group>
     </div>
@@ -81,13 +96,13 @@ import { EventBus } from "../index.js";
 export default {
   name: "mygo",
   components: {
-    vueBaberrage,
+    vueBaberrage
   },
   computed: {
     ...mapState({
-      account: (state) => state.account,
-      users: (state) => state.users.all,
-      games: (state) => state.games,
+      account: state => state.account,
+      users: state => state.users.all,
+      games: state => state.games
     }),
     b_playTime: function() {
       let time = this.BL;
@@ -100,7 +115,7 @@ export default {
       var min = Math.floor(time / 60);
       var sec = Math.round(time) % 60;
       return min + ":" + (sec < 10 ? "0" + sec : sec) + "";
-    },
+    }
   },
   props: {
     total_time: String,
@@ -108,7 +123,7 @@ export default {
     whiteOne: String,
     blackTwo: String,
     whiteTwo: String,
-    gameStatus: String,
+    gameStatus: String
   },
   data() {
     return {
@@ -128,15 +143,15 @@ export default {
       b1_turn: false,
       w1_turn: false,
       b2_turn: false,
-      w2_turn: false,
+      w2_turn: false
     };
   },
   watch: {
     BL: function(newValue, oldValue) {},
-    WL: function(newValue, oldValue) {},
+    WL: function(newValue, oldValue) {}
   },
   methods: {
-    ...mapMutations("games", ["updateGame", "updateNavTitle"]),
+    ...mapMutations("games", ["updateGame", "updateNavTitle"])
   },
   sockets: {
     get_message(data) {
@@ -146,7 +161,7 @@ export default {
         msg: data.username + " => " + data.message,
         time: 10,
         type: MESSAGE_TYPE.NORMAL,
-        barrageStyle: "normal",
+        barrageStyle: "normal"
       });
     },
     joinlobby(user) {
@@ -156,7 +171,7 @@ export default {
         msg: "系统: " + user + "进入对局室",
         time: 10,
         type: MESSAGE_TYPE.NORMAL,
-        barrageStyle: "red",
+        barrageStyle: "red"
       });
     },
     leavelobby(data) {
@@ -166,7 +181,7 @@ export default {
         msg: "系统: " + data + "离开对局室",
         time: 10,
         type: MESSAGE_TYPE.NORMAL,
-        barrageStyle: "red",
+        barrageStyle: "red"
       });
     },
     view(msg) {
@@ -177,20 +192,20 @@ export default {
       initGameData(this.account.user.name, this.games.game);
       initResumeGame(this.$refs.player, this.games.game, this.games.result);
       document.getElementById("wgo-control").style.display = "";
-    },
+    }
   },
   mounted() {
     let that = this;
     this.BL = this.total_time;
     this.WL = this.total_time;
-    EventBus.$on("w_timeout", (value) => {
+    EventBus.$on("w_timeout", value => {
       that.WL = value;
     });
-    EventBus.$on("b_timeout", (value) => {
+    EventBus.$on("b_timeout", value => {
       that.BL = value;
     });
 
-    EventBus.$on("myturn", (value) => {
+    EventBus.$on("myturn", value => {
       if (value == 0) {
         that.b1_turn = true;
         that.b2_turn = false;
@@ -202,7 +217,6 @@ export default {
         that.b2_turn = false;
         that.w1_turn = true;
         that.w2_turn = false;
-        
       }
       if (value == 2) {
         that.b1_turn = false;
@@ -227,10 +241,10 @@ export default {
         blackOne: this.blackOne,
         blackTwo: this.blackTwo,
         whiteOne: this.whiteOne,
-        whiteTwo: this.whiteTwo,
+        whiteTwo: this.whiteTwo
       });
     }
-  },
+  }
 };
 </script>
 <style scoped>
