bb90af5790ee67fa55db6c4c976bbbc04af751db
minikiller <minikiller@qq.com>
2020-06-14 22:38:08 +0800

    finish timer testing

--- src/component/timer.vue
+++ src/component/timer.vue
@@ -1,8 +1,24 @@
 <template>
   <div>
-    <span v-show="show" @click="getCode">获取验证码</span>
+    <span @click="getCode">获取验证码</span>
+    <span @click="stopCode">stop</span>
     <span v-show="!show" class="count">时间: 00:00:{{ count }} s</span>
     <span v-show="!show" class="count">读秒: 30秒 {{ numbers }}次</span>
+    <audio id="audioMinute0" src="/static/voice/0.mp3" preload="auto"></audio>
+    <audio id="audioMinute1" src="/static/voice/1.mp3" preload="auto"></audio>
+    <audio id="audioMinute2" src="/static/voice/2.mp3" preload="auto"></audio>
+    <audio id="audioMinute3" src="/static/voice/3.mp3" preload="auto"></audio>
+    <audio id="audioMinute4" src="/static/voice/4.mp3" preload="auto"></audio>
+    <audio id="audioMinute5" src="/static/voice/5.mp3" preload="auto"></audio>
+    <audio id="audioMinute6" src="/static/voice/6.mp3" preload="auto"></audio>
+    <audio id="audioMinute7" src="/static/voice/7.mp3" preload="auto"></audio>
+    <audio id="audioMinute8" src="/static/voice/8.mp3" preload="auto"></audio>
+    <audio id="audioMinute9" src="/static/voice/9.mp3" preload="auto"></audio>
+    <audio id="audioMinute10" src="/static/voice/10.mp3" preload="auto"></audio>
+    <audio id="audioMinute20" src="/static/voice/20.mp3" preload="auto"></audio>
+    <audio id="audioMinuteBegin" src="/static/voice/begin.mp3" preload="auto"></audio>
+    <audio id="audioMinuteEnd1" src="/static/voice/end1.mp3" preload="auto"></audio>
+    <audio id="audioMinuteEnd2" src="/static/voice/end2.mp3" preload="auto"></audio>
   </div>
 </template>
 
@@ -15,31 +31,56 @@ export default {
       show: true,
       count: "",
       timer: null,
-      numbers: 1,
+      numbers: 3,
+      time_count: 30
     };
   },
   methods: {
+    stopCode() {
+      clearInterval(this.timer);
+      this.count = this.time_count;
+      this.timer = null;
+    },
     getCode() {
-      const TIME_COUNT = 30;
+      document.getElementById("audioMinuteBegin").play();
       if (!this.timer) {
-        this.count = TIME_COUNT;
+        this.count = this.time_count;
         this.show = false;
         this.timer = setInterval(() => {
-          if (this.count > 0 && this.count <= TIME_COUNT) {
+          if (this.count > 0 && this.count <= this.time_count) {
             this.count--;
-            if (this.count == 0 && this.numbers > 0) {
-              this.numbers--;
-              this.count = TIME_COUNT;
+            if (this.count == 20)
+              document.getElementById("audioMinute20").play();
+            else if (this.count == 10)
+              document.getElementById("audioMinute10").play();
+            else if (this.count >= 0 && this.count < 10) {
+              document.getElementById("audioMinute" + this.count).play();
             }
-          } else {
-            this.show = true;
-            clearInterval(this.timer);
-            this.timer = null;
-            alert("you failed");
+            if (this.count == 0 && this.numbers == 3) {
+              this.numbers--;
+              this.count = this.time_count;
+              setTimeout(
+                "document.getElementById('audioMinuteEnd2').play()",
+                1000
+              );
+            } else if (this.count == 0 && this.numbers == 2) {
+              this.numbers--;
+              this.count = this.time_count;
+              setTimeout(
+                "document.getElementById('audioMinuteEnd1').play()",
+                1000
+              );
+            } else if (this.count == 0 && this.numbers == 1) {
+              // this.show = true;
+              this.numbers--;
+              clearInterval(this.timer);
+              this.timer = null;
+              alert("you failed");
+            }
           }
         }, 1000);
       }
-    },
-  },
+    }
+  }
 };
 </script>
