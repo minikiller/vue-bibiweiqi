be62cc826ecbb5f8a72543911d53d1e22f5b26b3
minikiller <minikiller@qq.com>
2020-08-23 12:37:33 +0800

    upgrade more

--- src/kifu/KifuViewPage.vue
+++ src/kifu/KifuViewPage.vue
@@ -26,7 +26,7 @@
           <b-form-input type="range" min="0" :max="maxStep" v-model="steps" ref="input"></b-form-input>
         </b-input-group>
         <!-- <div>{{ kifu.kifu_data }}</div> -->
-        <div>当前手数： {{ steps }}</div>
+        <div>当前手数: {{ steps }};行棋人: {{ currentTurn }}</div>
         <div style="width: 100%; margin: 0" ref="player" class="mt-3"></div>
       </b-col>
       <b-col sm="12" md="6">
@@ -41,8 +41,22 @@
           align="center"
         >
           <b-card-text>
-            <p>黑方:{{kifu.black_info}}</p>
-            <p>白方:{{kifu.white_info}}</p>
+            <p>
+              <i v-if="b1_turn" class="fas fa-user"></i>
+              黑方1:{{black1}}
+            </p>
+            <p>
+              <i v-if="w1_turn" class="fas fa-user"></i>
+              白方1:{{white1}}
+            </p>
+            <p>
+              <i v-if="b2_turn" class="fas fa-user"></i>
+              黑方2:{{black2}}
+            </p>
+            <p>
+              <i v-if="w2_turn" class="fas fa-user"></i>
+              白方2:{{white2}}
+            </p>
             <p>结果:{{kifu.result}}</p>
             <p>时间:{{kifu.create_date}}</p>
             <p v-html="result"></p>
@@ -82,11 +96,21 @@ export default {
       kifu: Object,
       endText: "开始数目",
       result: "",
+      black1: "",
+      black2: "",
+      white1: "",
+      white2: "",
+      b1_turn: false,
+      b2_turn: false,
+      w1_turn: false,
+      w2_turn: false,
+      currentTurn: "",
     };
   },
   mounted() {
     this.kifu = this.$route.params.game;
     if (this.kifu) {
+      this.getGameinfo();
       kifuViewGame(this.$refs.player, this.kifu);
       document.getElementById("wgo-control").style.display = "";
       this.maxStep = getTotalStep();
@@ -100,6 +124,7 @@ export default {
     }
     EventBus.$on("go_move", (msg) => {
       this.steps = msg;
+      this.getTurn(msg);
     });
     EventBus.$on("showScore", (msg) => {
       this.result = msg;
@@ -107,6 +132,48 @@ export default {
   },
   methods: {
     ...mapMutations("alert", ["success", "error", "clear"]),
+    //显示当前的落子是谁下的
+    getTurn(steps) {
+      if (steps == 0) {
+        this.currentTurn = "";
+        return;
+      }
+      var turn = parseInt(steps) % 4;
+      EventBus.$emit("myturn", turn);
+      if (turn == 0) {
+        this.b1_turn = false;
+        this.b2_turn = false;
+        this.w1_turn = false;
+        this.w2_turn = true;
+        this.currentTurn = this.white2;
+      } else if (turn == 1) {
+        this.b1_turn = true;
+        this.b2_turn = false;
+        this.w1_turn = false;
+        this.w2_turn = false;
+        this.currentTurn = this.black1;
+      } else if (turn == 2) {
+        this.b1_turn = false;
+        this.b2_turn = false;
+        this.w1_turn = true;
+        this.w2_turn = false;
+        this.currentTurn = this.white1;
+      } else if (turn == 3) {
+        this.b1_turn = false;
+        this.b2_turn = true;
+        this.w1_turn = false;
+        this.w2_turn = false;
+        this.currentTurn = this.black2;
+      }
+    },
+    getGameinfo() {
+      var black = this.kifu.black_info.split("&");
+      this.black1 = black[0];
+      this.black2 = black[1];
+      var white = this.kifu.white_info.split("&");
+      this.white1 = white[0];
+      this.white2 = white[1];
+    },
     score() {
       if (this.endText == "开始数目") {
         this.endText = "结束数目";
